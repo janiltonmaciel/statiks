5fd67327dca955aeb7e9b05ed2d809a1dd2f0c7c
Janilton Maciel <janilton@gmail.com>
2018-07-01 19:15:40 -0300

    Config cors

---
+++ lib/header.go
@@ -0,0 +1,46 @@
+package lib
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rs/cors"
+)
+
+func getCors(config statiksConfig) *cors.Cors {
+	return cors.New(cors.Options{
+		AllowedOrigins:   config.origins,
+		AllowedMethods:   config.methods,
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	})
+}
+
+var noCacheHeaders = map[string]string{
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+	"Expires":         "0",
+}
+
+func NoCacheHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range noCacheHeaders {
+			w.Header().Set(k, v)
+		}
+
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func CacheHandler(h http.Handler, maxAge string) http.Handler {
+	v := fmt.Sprintf("max-age=%s", maxAge)
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", v)
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
