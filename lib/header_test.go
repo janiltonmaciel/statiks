296d23877505cb99e51edbeb744e11bf597b52fa
Janilton Maciel <janilton@gmail.com>
2018-07-07 21:50:22 -0300

    Add CacheHandler and NoCacheHandler test

--- lib/header_test.go
+++ lib/header_test.go
@@ -1,10 +1,13 @@
 package lib
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
 	"github.com/rs/cors"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_getCors(t *testing.T) {
@@ -67,6 +70,50 @@ func Test_getCors(t *testing.T) {
 			if got := getCors(tt.args.config); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getCors() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestNoCacheHandler(t *testing.T) {
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	w := httptest.NewRecorder()
+
+	handler := NoCacheHandler(testHandler)
+	handler.ServeHTTP(w, nil)
+
+	for k, v := range noCacheHeaders {
+		assert.Equal(t, w.Header().Get(k), v)
+	}
+}
+
+func TestCacheHandler(t *testing.T) {
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	type args struct {
+		h      http.Handler
+		maxAge string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"Nocache",
+			args{testHandler, "0"},
+			"max-age=0",
+		},
+		{
+			"MaxAge",
+			args{testHandler, "99"},
+			"max-age=99",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			handler := CacheHandler(tt.args.h, tt.args.maxAge)
+			handler.ServeHTTP(w, nil)
+			assert.Equal(t, w.Header().Get("Cache-Control"), tt.want)
 		})
 	}
 }
