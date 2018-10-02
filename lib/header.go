66fa680fc56b8a66bcab2ccddb930c6aba0909f9
Janilton Maciel <janilton@gmail.com>
2018-10-02 03:29:53 -0300

    Add DelayHandler

--- lib/header.go
+++ lib/header.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/rs/cors"
 )
@@ -28,7 +29,6 @@ func NoCacheHandler(h http.Handler) http.Handler {
 		for k, v := range noCacheHeaders {
 			w.Header().Set(k, v)
 		}
-
 		h.ServeHTTP(w, r)
 	}
 
@@ -39,6 +39,15 @@ func CacheHandler(h http.Handler, maxAge string) http.Handler {
 	v := fmt.Sprintf("max-age=%s", maxAge)
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", v)
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func DelayHandler(h http.Handler, delay time.Duration) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
 		h.ServeHTTP(w, r)
 	}
 
